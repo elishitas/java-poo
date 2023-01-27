5f2b34a85b821fe0c1bbe4fa7b563f256ff84695
Elishitas <arayamariaelisa@gmail.com>
2023-01-27 16:42:51 -0300

    Poo-clases

---
+++ 21-ClasesPOO/src/Logica/ClasesPOO.java
@@ -0,0 +1,24 @@
+/*
+Clases
+Es una plantilla o molde que permite contruir objetos
+Si quiero traer un objeto de la vida real y transformarlo al mundo logica hay que pensar cuales son sus atributos, caracteristicas como para hacer un molde.
+Representan "entidades" de la vida real
+Poseen atributos y metodos
+No pueden ser utilizados directamente(sino mediante instancias a objetos)
+ */
+package Logica;
+
+/**
+ *
+ * @author eli
+ */
+public class ClasesPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ 21-POO/src/pkg21/poo/POO.java
@@ -0,0 +1,30 @@
+/*
+Paradigma
+Paradigma orientada a objetos -> Normas / Estandares
+Todo objeto de la vida real puede ser abstraido de la realidad y se los puede representar de moda logicca en un programa
+Los objetos pueden comunicarse mendiante mensajes
+
+Se base en concepto -> Agrupar codigo que se relaciona en un mismo lugar
+Ej: Caja -> De Cocina / Caja -> De dormitorio -> Estas cajas son conocidas como CLASES
+Abstracion -> Posibilidad de traer algo del mundo real
+Encapsulamiento ->
+Polimorfimos >
+Herencia ->
+UML -> Lenguaje universal (o unificado) de modelado. 
+ */
+package pkg21.poo;
+
+/**
+ *
+ * @author eli
+ */
+public class POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

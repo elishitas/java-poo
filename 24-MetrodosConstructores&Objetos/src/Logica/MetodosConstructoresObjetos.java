8c4c9e663ee6eb94d3a64904a0a09aca4371abbf
Elishitas <arayamariaelisa@gmail.com>
2023-01-30 16:41:22 -0300

    correccion main

---
+++ 24-MetrodosConstructores&Objetos/src/Logica/MetodosConstructoresObjetos.java
@@ -0,0 +1,18 @@
+/*
+Un objeto es una instancia de una clase
+Posee un estado de acuerdo a sus atributos
+Posee un comportamiento -realiza operaciones de acuerdo a sus metodos-
+ */
+package Logica;
+
+public class MetrodosConstructoresObjetos {
+
+    public static void main(String[] args) {
+          
+        Alumno alumno1 = new Alumno();
+        Alumno alumno2 = new Alumno(34, "Lisa", "Ara");
+                
+    }
+    
+}
+
